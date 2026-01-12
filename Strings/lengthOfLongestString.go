e3e929a701e8504914be6febeb30b8810226ada6
Linux-DEX <sarabjeet.9353@gmail.com>
2026-01-12 07:29:15 +0530

    string: length of longest string

--- Strings/lengthOfLongestString.go
+++ Strings/lengthOfLongestString.go
@@ -4,10 +4,96 @@ import "fmt"
 
 type LengthOfLongestSubstring struct{}
 
+// Brute Force (O(n³))
+func (l LengthOfLongestSubstring) Brute(s string) int {
+	n := len(s)
+	maxLen := 0
+
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			if l.allUnique(s, i, j) {
+				if j-i+1 > maxLen {
+					maxLen = j - i + 1
+				}
+			}
+		}
+	}
+
+	return maxLen
+}
+
+func (l LengthOfLongestSubstring) allUnique(s string, start, end int) bool {
+	seen := make(map[byte]bool)
+	for i := start; i <= end; i++ {
+		if seen[s[i]] {
+			return false
+		}
+		seen[s[i]] = true
+	}
+	return true
+}
+
+// Better (O(n²))
+func (l LengthOfLongestSubstring) Better(s string) int {
+	n := len(s)
+	maxLen := 0
+
+	for i := 0; i < n; i++ {
+		seen := make(map[byte]bool)
+		for j := i; j < n; j++ {
+			if seen[s[j]] {
+				break
+			}
+			seen[s[j]] = true
+			if j-i+1 > maxLen {
+				maxLen = j - i + 1
+			}
+		}
+	}
+
+	return maxLen
+}
+
+// (Sliding Window) Optimal (O(n))
+func (l LengthOfLongestSubstring) Optimal(s string) int {
+	lastIndex := make(map[byte]int)
+	start := 0
+	maxLen := 0
+
+	for end := 0; end < len(s); end++ {
+		if idx, found := lastIndex[s[end]]; found && idx >= start {
+			start = idx + 1
+		}
+
+		lastIndex[s[end]] = end
+		if end-start+1 > maxLen {
+			maxLen = end - start + 1
+		}
+	}
+
+	return maxLen
+}
 
 func main() {
 	l := LengthOfLongestSubstring{}
-	str := "abcabcbb"
+	str1 := "abcabcbb"
+	str2 := "bbbbb"
+	str3 := "pwwkew"
 
-	fmt.Println("LengthOfLongestSubstring")
+	fmt.Println("string: ", str1)
+	fmt.Println("Brute:", l.Brute(str1))
+	fmt.Println("Better:", l.Better(str1))
+	fmt.Println("Optimal:", l.Optimal(str1))
+	fmt.Println("\n---\n")
+
+	fmt.Println("string: ", str2)
+	fmt.Println("Brute:", l.Brute(str2))
+	fmt.Println("Better:", l.Better(str2))
+	fmt.Println("Optimal:", l.Optimal(str2))
+	fmt.Println("\n---\n")
+
+	fmt.Println("string: ", str3)
+	fmt.Println("Brute:", l.Brute(str3))
+	fmt.Println("Better:", l.Better(str3))
+	fmt.Println("Optimal:", l.Optimal(str3))
 }
